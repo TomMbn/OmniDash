e10a0f19bc4a64791bba97d2f0a339085e5871f8
TomMbn <tom.mauboussin@icloud.com>
2026-07-05 02:24:36 +0200

    fix: nettoie la clé VAPID (espaces/guillemets) avant conversion push

--- src/views/WindowsView.vue
+++ src/views/WindowsView.vue
@@ -197,8 +197,9 @@ const syncNetatmo = async (code = null) => {
 }
 
 const urlBase64ToUint8Array = (base64String) => {
-  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
-  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
+  const cleaned = base64String.trim().replace(/^["']|["']$/g, '')
+  const padding = '='.repeat((4 - (cleaned.length % 4)) % 4)
+  const base64 = (cleaned + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = atob(base64)
   return Uint8Array.from([...rawData].map((c) => c.charCodeAt(0)))
 }
